139ca95fb88f0e057d29d1e5e3d8bb0d49a83662
GituAja <Gituaja.copyright@Gmail.com>
2016-12-04 08:17:57 +0700

    home sudah bisa lanjutkan isinya

--- app/src/main/java/id/sch/smktelkom_mlg/project/xirpl204132231/infomalang/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xirpl204132231/infomalang/MainActivity.java
@@ -1,7 +1,9 @@
 package id.sch.smktelkom_mlg.project.xirpl204132231.infomalang;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +11,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.imageButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this , SejarahActivity.class));
+            }
+        });
+
+        findViewById(R.id.imageButton2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this , WisataActivity.class));
+            }
+        });
+        findViewById(R.id.imageButton3).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this , KulinerActivity.class));
+            }
+        });
+        findViewById(R.id.imageButton4).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this , BudayaActivity.class));
+            }
+        });
     }
 }
-
