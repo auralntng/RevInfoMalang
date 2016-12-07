17a75abf31c23ffb5ac7c295cec89446a5f75ccf
GituAja <Gituaja.copyright@Gmail.com>
2016-12-07 18:17:26 +0700

    part 2/3 onback updated with new icon InfoMalang

--- app/src/main/java/id/sch/smktelkom_mlg/project/xirpl204132231/infomalang/KulinerActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xirpl204132231/infomalang/KulinerActivity.java
@@ -2,6 +2,7 @@ package id.sch.smktelkom_mlg.project.xirpl204132231.infomalang;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class KulinerActivity extends AppCompatActivity {
 
@@ -9,5 +10,14 @@ public class KulinerActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_kuliner);
+
+        findViewById(R.id.buttonback).setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                onBackPressed();
+            }
+        });
     }
 }
--- app/src/main/java/id/sch/smktelkom_mlg/project/xirpl204132231/infomalang/SejarahActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xirpl204132231/infomalang/SejarahActivity.java
@@ -1,11 +1,7 @@
 package id.sch.smktelkom_mlg.project.xirpl204132231.infomalang;
 
-import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.os.Bundle;
 
 public class SejarahActivity extends AppCompatActivity {
 
@@ -13,7 +9,5 @@ public class SejarahActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sejarah);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
     }
 }
