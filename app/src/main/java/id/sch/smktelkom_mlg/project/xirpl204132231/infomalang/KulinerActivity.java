b54ce688c87da87e8787fb7552f9945cc0e8f3de
indradv <contact.krisnapf@gmail.com>
2016-12-07 22:34:24 +0700

    3/3  onback updated with new icon InfoMalang

--- app/src/main/java/id/sch/smktelkom_mlg/project/xirpl204132231/infomalang/KulinerActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xirpl204132231/infomalang/KulinerActivity.java
@@ -2,6 +2,7 @@ package id.sch.smktelkom_mlg.project.xirpl204132231.infomalang;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 
 public class KulinerActivity extends AppCompatActivity {
@@ -9,15 +10,17 @@ public class KulinerActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.setTitle("Kuliner");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         setContentView(R.layout.activity_kuliner);
 
-        findViewById(R.id.buttonback).setOnClickListener(new View.OnClickListener()
-        {
-            @Override
-            public void onClick(View v)
-            {
-                onBackPressed();
-            }
-        });
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            onBackPressed();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
