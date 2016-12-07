8decc0d2c2dd85641aeb55573bd94257898858a0
indradv <contact.krisnapf@gmail.com>
2016-12-07 20:55:36 +0700

    all back button already clear

--- app/src/main/java/id/sch/smktelkom_mlg/project/xirpl204132231/infomalang/SejarahActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xirpl204132231/infomalang/SejarahActivity.java
@@ -2,6 +2,7 @@ package id.sch.smktelkom_mlg.project.xirpl204132231.infomalang;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class SejarahActivity extends AppCompatActivity {
 
@@ -9,5 +10,13 @@ public class SejarahActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sejarah);
+        findViewById(R.id.buttonback).setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                onBackPressed();
+            }
+        });
     }
 }
--- app/src/main/java/id/sch/smktelkom_mlg/project/xirpl204132231/infomalang/WisataActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xirpl204132231/infomalang/WisataActivity.java
@@ -2,6 +2,7 @@ package id.sch.smktelkom_mlg.project.xirpl204132231.infomalang;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class WisataActivity extends AppCompatActivity {
 
@@ -9,5 +10,13 @@ public class WisataActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wisata);
+        findViewById(R.id.buttonback).setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                onBackPressed();
+            }
+        });
     }
 }
